d75c55b2bae76765a5cc07524333f3e4d2bea84f
abbosbek23 <muratovabbosbek@gmail.com>
2024-05-17 22:53:27 +0500

    add page

---
+++ src/home/Components/testimonals.jsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+import { Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+
+const Testimonals = () => {
+  return (
+    <Box>
+        <Box sx={{ display: "grid",placeItems: "center", marginTop:"150px"}}>
+     <Typography sx={{textTransform:"uppercase",color:"#D3874A"}}>
+     testimonials
+     </Typography>
+     <Typography sx={{fontSize:"40px",width:'520px',textAlign:"center"}}>
+     See what our clients have to say about us
+     </Typography>
+     <Box>
+        <Grid container spacing={2}>
+         
+        </Grid>
+     </Box>
+        </Box>
+    </Box>
+  )
+}
+
+export default Testimonals
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,7 +1,13 @@
 // import React from 'react
-import { Grid, Container, Paper, Typography, Box } from "@mui/material"
+import { Grid, Container, Typography, Box } from "@mui/material"
 import Navbar from "../components/Navbar"
-import Slider from "./slider"
+import Slider from "./Components/slider"
+
+import AdditionalBox from "./Components/additionalbox"
+import Perfection from "./Components/perfection"
+import Benefit from "./Components/benefit"
+import Testimonals from "./Components/testimonals"
+
 
 const Home = () => {
   return (
@@ -20,9 +26,34 @@ const Home = () => {
         <Grid item xs={6} >
           <Slider/>
         </Grid>
-        {/* Yana grid elementlarni qo'shishingiz mumkin */}
+    
       </Grid>
     </Container>
+    <Box sx={{marginTop:"120px"}}>
+      <Box sx={{width:"100%",display:"block",alignItems:"center",textAlign:"center"}}>
+      <Typography sx={{textAlign:"center",color:"#D3874A"}}>
+      Unforgettable
+      </Typography>
+      <Typography sx={{color:"#252A2E",fontSize:"40px",marginBlock:"10px",marginLeft:"28%",width:"600px"}}>
+      Creating Your Dream Wedding Experience
+      </Typography>
+      <Typography sx={{textAlign:"center",width:"770px",marginLeft:"21%"}}>
+      At Dream Day, we offer a range of services to make your wedding day truly special. From helping you select the perfect venue to managing all aspects of catering and designing the decor, we are here to bring your dream wedding to life.
+      </Typography>
+      </Box>
+      <Box>
+      <AdditionalBox/>
+      </Box>
+      <Box>
+        <Perfection/>
+      </Box>
+      <Box sx={{padding:"112px 64px",backgroundColor:"#757B67"}}>
+        <Benefit/>
+      </Box>
+      <Box>
+        <Testimonals/>
+      </Box>
+    </Box>
     </div>
   )
 }
