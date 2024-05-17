66e9370d05f643dbcb8bd8504fe08e078b890235
abbosbek23 <muratovabbosbek@gmail.com>
2024-05-17 11:09:39 +0500

    add slider

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,6 +1,7 @@
 // import React from 'react
-import { Grid, Container, Paper, Typography } from "@mui/material"
-import Navbar from "../../components/Navbar"
+import { Grid, Container, Paper, Typography, Box } from "@mui/material"
+import Navbar from "../components/Navbar"
+import Slider from "./slider"
 
 const Home = () => {
   return (
@@ -8,16 +9,16 @@ const Home = () => {
         <Navbar/>
         <Container>
       <Grid container spacing={2}>
-        <Grid item xs={6} sx={{marginTop:"100px"}}>
-          <Typography>
+        <Grid item xs={6} paddingLeft={0} style={{paddingLeft:"0px"}}>
+          <Typography sx={{fontSize:"48px",marginTop:"150px"}}>
           Your Love Story, Our Masterpiece
           </Typography>
-          <Typography>
+          <Typography sx={{fontSize:"16px",color:"#252A2E"}}>
           Dream Day is your trusted partner in turning your wedding dreams into reality. With our expertise in wedding planning, we ensure every detail is taken care of, so you can relax and enjoy your special day.
           </Typography>
-        </Grid>
-        <Grid item xs={6} sx={{marginTop:"100px"}}>
-          <Paper>Item 2</Paper>
+        </Grid> 
+        <Grid item xs={6} >
+          <Slider/>
         </Grid>
         {/* Yana grid elementlarni qo'shishingiz mumkin */}
       </Grid>
